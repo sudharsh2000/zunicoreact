c90786b4e5ba860d678792ff563e248df40b6d70
apple <apple@apples-MacBook-Pro.local>
2025-10-24 13:34:18 +0530

    edited

--- src/Components/Rowitems.jsx
+++ src/Components/Rowitems.jsx
@@ -38,13 +38,14 @@ function Rowitems({ali_type,Title}) {
     <div>
         <div className='bg-white p-2 my-4'>
         <h1 className='text-lg md:text-2xl ml-4 md:ml-8 my-2 md:my-8 font-bold overflow-auto'>{Title}</h1>
-     {
+     {ali_type==='row'&&
      <button onClick={()=>scrollfn('left')}  className='    bg-[#baa5a56a] absolute left-[1rem] md:left-[3.5rem] w-[1.6rem] h-[2rem] md:w-[2rem] md:h-[6rem] rounded-sm z-index:10 hover:bg-red-200 hover:scale-125 duration-300 transform-3d  transition-transform  shadow-2xl'>&#9664;</button>
        }
         {
+          ali_type==='row'&&
           <button onClick={()=>scrollfn('right')}  className='  bg-[#baa5a56a] absolute right-[1rem] md:right-[3.5rem]  w-[1.6rem] h-[2rem] md:w-[2rem] md:h-[6rem] rounded-sm z-index:10 hover:bg-red-200 hover:scale-125 duration-300 transform-3d  transition-transform  shadow-2xl'>&#9654;</button>
 }
-        <div ref={ref} className={`flex ml-[1.3rem] mr-[1rem]  md:mr-[3rem] md:ml-[4rem] p-2 md:p-4 gap-[.5rem] md:gap-[5.5rem] ${ali_type==='row'?'overflow-x-auto':'flex-wrap'}  flex-1 whitespace-nowrap no-scrollbar`}>
+        <div ref={ref} className={`flex ml-[1.3rem] mr-[1rem]  md:mr-[3rem] md:ml-[4rem] p-2 md:p-4 gap-[.5rem]  ${ali_type==='row'?'overflow-x-auto md:gap-[6.5rem] ':'flex-wrap md:gap-[4.5rem]'}  flex-1 whitespace-nowrap no-scrollbar`}>
     
     
         {products &&
