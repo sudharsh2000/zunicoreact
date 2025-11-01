a405ece4c665e19b604cf5c16b7627eab9d7bc6d
apple <apple@apples-MacBook-Pro.local>
2025-11-02 00:04:29 +0530

    new one

--- src/Redux/api.jsx
+++ src/Redux/api.jsx
@@ -1,7 +1,7 @@
 
 
 
-export const homeapi=process.env.NODE_ENV === 'production'?'http://127.0.0.1:8000/':'https://zunico.onrender.com/'
+export const homeapi=process.env.NODE_ENV === 'production'?'https://zunico.onrender.com/':'http://127.0.0.1:8000/'
 export const signupapi=homeapi+'api/signup/'
 export const signinapi=homeapi+'signin'
 export const refreshapi=homeapi+'Refresh/'
