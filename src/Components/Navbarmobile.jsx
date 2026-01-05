53c505272903bdf2992e592faf896d6422798c2e
apple <apple@apples-MacBook-Pro.local>
2026-01-05 11:31:01 +0530

    new

--- src/Components/Navbarmobile.jsx
+++ src/Components/Navbarmobile.jsx
@@ -13,8 +13,7 @@ function Navbarmobile() {
     <div className=' border-gray-100 rounded-t-xl shadow-lg md:hidden py-3 px-4 fixed min-h-[2.2rem] w-full self-center  z-30 bg-white  bottom-0 border-t-1 '>
     <ul className=' flex  flex-row justify-between  items-center '>
         <li onClick={()=>navigate('/')} className={`text-xs flex flex-col  justify-center items-center gap-2 ${location.pathname==='/'?'font-extrabold':''}`} ><Home className={`h-[2rem] w-[4rem] py-1 px-2 rounded-2xl ${location.pathname==='/'?'bg-emerald-100':'bg-white'}`}/> Home</li>
-        <li onClick={()=>{
-        userInfo?.superuser?navigate('/admin/dashboard'):navigate('/profile')
+        <li onClick={()=>{navigate('/profile')
           } } className={`text-xs flex flex-col justify-center items-center gap-2 ${location.pathname==='/profile'?'font-extrabold':''}`} ><User className={`h-[2rem] w-[4rem] py-1 px-2 rounded-2xl ${location.pathname==='/profile'?'bg-emerald-100':'bg-white'}`} /> Account</li>
         <li onClick={()=>navigate('/cart')} className={`text-xs flex flex-col justify-center  items-center gap-2 ${location.pathname==='/cart'?'font-extrabold':''}`}><ShoppingCart className={`h-[2rem] w-[4rem] py-1 px-2 rounded-2xl ${location.pathname==='/cart'?'bg-emerald-100':'bg-white'}`}/> Cart</li>
           <li onClick={()=>navigate('/orders')} className={`text-xs flex flex-col justify-center  items-center gap-2 ${location.pathname==='/orders'?'font-extrabold':''}`}><Store className={`h-[2rem] w-[4rem] py-1 px-2 rounded-2xl ${location.pathname==='/orders'?'bg-emerald-100':'bg-white'}`}/> Orders</li>
