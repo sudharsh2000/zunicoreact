9c2aacbe701f74d626be6f7db43f626822c06995
apple <apple@apples-MacBook-Pro.local>
2026-01-27 12:13:53 +0530

    new

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,13 @@ import {VitePWA} from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-    tailwindcss(), 
+    esbuild: {
+    supported: {
+      'top-level-await': true
+    }
+  },
+    tailwindcss(),
+     
       VitePWA({
       registerType: 'autoUpdate',
 
